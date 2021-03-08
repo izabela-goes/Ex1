537ca0f85a163e601890367615d2c875d590456f
izabela-goes <izabelagoes1@gmail.com>
2021-03-08 20:55:48 -0300

    exercicios

---
+++ VerifIMC/Eq2Grau.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerifIMC
+{
+    class Eq2Grau
+    {
+        public static void Main(string[] args)  
+        {
+            float A, B, C, delta;
+            float x1 = 0;
+            float x2 = 0;
+
+
+            Console.WriteLine(" ");
+            Console.Write("Informe o valor de A: ");
+            A = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(" ");
+            Console.Write("Informe o valor de  B: ");
+            B = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(" ");
+            Console.Write("Informe o valor de C: ");
+            C = Convert.ToInt32(Console.ReadLine());
+
+            if (A == 0)
+            {
+                Console.WriteLine("A NÃO PODE SER IGUAL A ZERO!");
+            }
+
+            delta = (float)(B * B - (4 * A * C));
+
+            if (delta < 0)
+            {
+                Console.WriteLine("DELTA NEGATIVO NÃO POSSUI RAÍZES REAIS");
+            }
+            else
+            {
+                x1 = (float)(-B + Math.Sqrt(delta)) / (2 * A);
+                x2 = (float)(-B - Math.Sqrt(delta)) / (2 * A);
+            }
+
+            Console.WriteLine("Valor x1: " + x1 + "\nValor x2: " + x2);
+
+        }
+    }
+    }
---
+++ VerifIMC/VerifIMC.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerifIMC
+{
+    class VerifIMC
+    {
+        static void Main(string[] args)
+        {
+            float imc, massa, altura;
+            string sMsg = "Resultado";
+
+            Console.WriteLine("Calculo do IMC");
+            Console.WriteLine();
+
+            Console.WriteLine("Informe o peso (kg): ");
+            massa = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe a altura (m): ");
+            altura = float.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            imc = (massa / (altura * altura));
+
+            if (imc < 18.5)
+                sMsg += " Peso abaixo do normal";
+            else if ((imc == 18.5) || (imc == 25))
+                sMsg += " Peso normal";
+            if ((imc > 25) || (imc == 30))
+                sMsg += " Sobrepeso";
+            else if ((imc > 30) || (imc ==35))
+                sMsg += " Grau de Obesidade 1";
+            if ((imc > 35) ||(imc == 40))
+                sMsg += " Grau de Obesidade 2";
+            else if (imc > 40)
+                sMsg += " Grau de Obesidade 3";
+            
+
+            Console.WriteLine(sMsg + " IMC = " + imc);
+
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
